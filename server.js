d3179936942614bdbe6e91a5f144a125479e0b34
Tanumoy Nandy <tmoy.nandy@gmail.com>
2017-08-04 00:50:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/main.js',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','main.js'));
+    res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 
 
