b1417e39cf7f89eff11d3f7d20461a1cc4067fa6
Tanumoy Nandy <tmoy.nandy@gmail.com>
2017-08-03 23:26:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
