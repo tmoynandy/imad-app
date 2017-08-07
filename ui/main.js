8672a137818a2e59830162364b7e27b3f09a00d7
Tanumoy Nandy <tmoy.nandy@gmail.com>
2017-08-07 19:11:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,54 @@ function loadabout(){
     req.open("GET", "http://tmoynandycourses.imad.hasura-app.io/ui/about.html", true);
   req.send();
 }
+
+function loadabout(){
+    //a click event occured jeta loadabout k call kore
+    //now create an XMLHttpRequest object
+    var req= new XMLHttpRequest();
+    //now every time there is a change in state of the XMLHttpRequest, a function call is made
+    // req.onreadystatechange detects change and calls function
+    req.onreadystatechange=function(){
+        
+      if(this.readyState==4 && this.status==200){
+          console.log('in 200')
+          document.getElementsByClassName("container")[0].innerHTML = this.responseText; //[0] because am calling elements..multiple gets returned but i want only one
+      }  
+    };
+    req.open("GET", "http://tmoynandycourses.imad.hasura-app.io/ui/about.html", true);
+  req.send();
+}
+
+function loadcontact(){
+    //a click event occured jeta loadabout k call kore
+    //now create an XMLHttpRequest object
+    var req= new XMLHttpRequest();
+    //now every time there is a change in state of the XMLHttpRequest, a function call is made
+    // req.onreadystatechange detects change and calls function
+    req.onreadystatechange=function(){
+        
+      if(this.readyState==4 && this.status==200){
+          console.log('in 200')
+          document.getElementsByClassName("container")[0].innerHTML = this.responseText;//[0] because am calling elements..multiple gets returned but i want only one
+      }  
+    };
+    req.open("GET", "http://tmoynandycourses.imad.hasura-app.io/ui/contact.html", true);
+  req.send();
+}
+
+function loadworks(){
+    //a click event occured jeta loadabout k call kore
+    //now create an XMLHttpRequest object
+    var req= new XMLHttpRequest();
+    //now every time there is a change in state of the XMLHttpRequest, a function call is made
+    // req.onreadystatechange detects change and calls function
+    req.onreadystatechange=function(){
+        
+      if(this.readyState==4 && this.status==200){
+          console.log('in 200')
+          document.getElementsByClassName("container")[0].innerHTML = this.responseText;//[0] because am calling elements..multiple gets returned but i want only one
+      }  
+    };
+    req.open("GET", "http://tmoynandycourses.imad.hasura-app.io/ui/works.html", true);
+  req.send();
+}
