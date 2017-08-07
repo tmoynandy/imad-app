aa81c3eb456ea9bd8487bba1ad228caf2965aa13
Tanumoy Nandy <tmoy.nandy@gmail.com>
2017-08-07 18:31:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,18 @@ function myFunction() {
     }
 }
 
-var about=document.getElementById("about");
-about.onclick=function(){
-    
-};
+function loadabout(){
+    //a click event occured jeta loadabout k call kore
+    //now create an XMLHttpRequest object
+    var req= new XMLHttpRequest();
+    //now every time there is a change in state of the XMLHttpRequest, a function call is made
+    // req.onreadystatechange detects change and calls function
+    req.onreadystatechange=function(){
+      if(this.readyState==4 && this.Status==200){
+          document.getElementsByClassName("container").innerHTML =
+      this.responseText;
+      }  
+    };
+    req.open("GET", "http://tmoynandycourses.imad.hasura-app.io/about.html", true);
+  req.send();
+}
