c7c51f1d8c94a95f2291651b6b954744e3aa8947
Tanumoy Nandy <tmoy.nandy@gmail.com>
2017-08-07 18:58:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,6 +15,7 @@ function loadabout(){
     //now every time there is a change in state of the XMLHttpRequest, a function call is made
     // req.onreadystatechange detects change and calls function
     req.onreadystatechange=function(){
+        console.log(this.Status)
       if(this.readyState==4 && this.Status==200){
           console.log('in 200')
           document.getElementsByClassName("container")[0].innerHTML = this.responseText;
