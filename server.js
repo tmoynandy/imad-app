7f05f2b851033e3961f185567ac4682f1ac919af
Tanumoy Nandy <tmoy.nandy@gmail.com>
2017-08-07 18:32:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-
+app.get('/ui/about.html',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','about.html'));
+});
 
 
 
