df900ed5ca4f16f32a53f91bced96dc4d732db06
Tanumoy Nandy <tmoy.nandy@gmail.com>
2017-08-30 07:58:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -72,7 +72,7 @@ function loadindex(){
         
       if(this.readyState==4 && this.status==200){
           console.log('in 200')
-          document.getElementsByClassName("container")[0].innerHTML = this.responseText;//[0] because am calling elements..multiple gets returned but i want only one
+          document.getElementsByClassName("body").innerHTML = this.responseText;//[0] because am calling elements..multiple gets returned but i want only one
       }  
     };
     req.open("GET", "http://tmoynandycourses.imad.hasura-app.io/ui/index.html" , true);
